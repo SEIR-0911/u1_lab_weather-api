f1e8e619d9e43d7d2be653de731ffb6e7cdd4e18
FrogFishCow <dmitrieliseev96@gmail.com>
2023-09-21 07:27:54 -0400

    updated with some media queries

--- script.js
+++ script.js
@@ -1,81 +1,64 @@
 const apiKey = '3d57896ece874c94ac1144657232009'
 const button = document.querySelector('#submitButton')
 const input = document.querySelector('#textInput')
-let cityName = document.querySelector('#cityName')
-let cityTemp = document.querySelector('#temp')
-let cityTime = document.querySelector('#cityTime')
-let cityHumidity = document.querySelector('#humidity')
-let citySky = document.querySelector('#sky')
-let cityWind = document.querySelector('#wind')
-let tempChange = document.querySelector('#celFer')
-let windChange = document.querySelector('#mph')
+let locationSearch = document.querySelector('#location')
+let todayDate = document.querySelector('#date')
+let todayWeather = document.querySelector('#information')
+let todayPicture = document.querySelector("#weather-pic")
+let tempClick = document.querySelector('#temperature')
+let windClick = document.querySelector('#wind')
+let humidityClick = document.querySelector('#humidity')
+let uVClick = document.querySelector("#uv-Index")
+let pressureClick = document.querySelector("#Pressure")
+let lastUpdated = document.querySelector("#last-update")
 
 button.addEventListener('click', async () => {
     let weatherInput = input.value
-    console.log(weatherInput)
-
+    
     let response = await axios.get(`http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${weatherInput}&aqi=no`)
     console.log(response)
 
     let cityLocation = `${response.data.location.name}, ${response.data.location.region}, ${response.data.location.country}`
-    cityName.innerText = cityLocation
+    locationSearch.innerText = cityLocation
 
     let cityDateTime = `${response.data.location.localtime}`
-    cityTime.innerText = cityDateTime
+    todayDate.innerText = cityDateTime
 
-    let cityWeather = `${response.data.current.temp_c} Degrees Celcius`
-    cityTemp.innerText = cityWeather
+    let cityWeather = `${response.data.current.temp_c} °C`
+    todayWeather.innerText = cityWeather
 
-    let cityHumidex =  `Humidity: ${response.data.current.humidity}`
-    cityHumidity.innerText = cityHumidex
+    let picWeather = response.data.current.condition.icon
+    document.getElementById("weather-pic").src=`${picWeather}`
 
-    let citySkyWeather = `It's ${response.data.current.condition.text} outside`
-    citySky.innerText = citySkyWeather
+    let lastUpdate = response.data.current.last_updated
+    lastUpdated.innerText = `Last update: ${lastUpdate}`
 
-    let CityWindKM = `The wind is ${response.data.current.wind_kph}kph`
-    cityWind.innerText = CityWindKM
 
-    windChange.addEventListener('click', async () => {
-        if (windChange.innerText === 'MPH') {
-        let CityWindKM = `The wind is ${response.data.current.wind_mph}mph`
-        cityWind.innerText = CityWindKM
-        windChange.innerText = 'KPH'
-        } else {
-        (windChange.innerText === 'MPH') 
-        let CityWindKM = `The wind is ${response.data.current.wind_kph}kph`
-        cityWind.innerText = CityWindKM
-        windChange.innerText = 'MPH'
-        }
-
+    tempClick.addEventListener('click', async () => {
+        let cityWeather = `${response.data.current.temp_c} °C`
+        todayWeather.innerText = cityWeather
     })
-    
-    
-    tempChange.addEventListener('click', async () => {
-        if (tempChange.innerText === 'F') {
-            let cityWeather = `${response.data.current.temp_f} Degrees Ferheeneneheit`
-            cityTemp.innerText = cityWeather
-            tempChange.innerText = 'C'
-        } else {
-            let cityWeather = `${response.data.current.temp_c} Degrees Celcius`
-            cityTemp.innerText = cityWeather
-            tempChange.innerText = 'F'
-        }
+
+    windClick.addEventListener('click', async () => {
+        let cityWind = `${response.data.current.wind_kph}kph,  ${response.data.current.wind_dir}`
+        todayWeather.innerText = cityWind
     })
-    
+
+    humidityClick.addEventListener('click', async () => {
+        let cityHumidex =  `${response.data.current.humidity}g/kg`
+        todayWeather.innerText = cityHumidex
+    })
+
+    uVClick.addEventListener('click', async () => {
+        let cityUV =  `${response.data.current.uv}`
+        todayWeather.innerText = cityUV
+    })
+
+    pressureClick.addEventListener('click', async () => {
+        let cityPressure =  `${response.data.current.pressure_in}`
+        todayWeather.innerText = cityPressure
+    })
+
+
 
 })
-
-
-//animations
-// @keyframes wind{
-//     0% {
-//       background-position: 0 200px, 0 300px, 100px 250px;
-//     }
-//     50% {
-//       background-position: -1000px -200px, -1200px -300px,-1100px -250px;
-//     }
-//     100% {
-//       background-position: 1000px 200px, 1200px 300px, 1100px 250px;
-//     }
-  
-//   }
